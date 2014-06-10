59693550061b0ef40d5e749ddc386237cd7d6bde
tasku12@yahoo.com <tasku12@yahoo.com>
2014-06-10 01:25:03 -0400

    Finished the most common specializations. Determine whether Matrix vector parameter constructors should be seperate functions. Begin implementing generic operations.

--- source/Common/ZMath.h
+++ source/Common/ZMath.h
@@ -21,25 +21,30 @@ namespace ZMath
 	struct Vector 
 	{ 
 		T data[n];
-		T operator[](unsigned int component) { return data[component]; };
+		T& operator[](unsigned int component) { return data[component]; };
 	};
 
-	// Partial Vector specializations with .x .y .z .w .r .g etc.. access
+	// Common Vector typedefs
+	typedef Vector<float, 2> Vec2;
+	typedef Vector<float, 3> Vec3;
+	typedef Vector<float, 4> Vec4;
+
+	// Partial Vector specializations with .x .y .z .w .r .b etc.. access
 	template<typename T>
 	struct Vector<T, 2>
 	{
 		union
 		{
-			float data[2];
-			struct { float x, y; };
-			struct { float r, g; };
+			T data[2];
+			struct { T x, y; };
+			struct { T r, g; };
 		};
 
 		// Constructors
 		Vector() : x(0), y(0) {}
 		Vector(T x, T y) : x(x), y(y) {}
-		explicit Vector<T, 2>(T value) : x(value), y(value) {}
-		float operator[](unsigned int component) { return data[component]; }
+		explicit Vector(T value) : x(value), y(value) {}
+		T& operator[](unsigned int component) { return data[component]; }
 	};
 
 	template<typename T>
@@ -56,7 +61,8 @@ namespace ZMath
 		Vector() : x(0), y(0), z(0) {}
 		Vector(T x, T y, T z) : x(x), y(y), z(z) {}
 		explicit Vector(T value) : x(value), y(value), z(value) {}
-		T operator[](unsigned int component) { return data[component]; }
+		Vector(Vec2& xy, T z) : x(xy.x), y(xy.y), z(z) {}
+		T& operator[](unsigned int component) { return data[component]; }
 	};
 
 	template<typename T>
@@ -73,13 +79,10 @@ namespace ZMath
 		Vector() : x(0), y(0), z(0), w(0) {}
 		Vector(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
 		explicit Vector(T value) : x(value), y(value), z(value), w(value) {}
-		T operator[](unsigned int component) { return data[component]; };
+		Vector(Vector<T, 2>& xy, T z, T w) : x(xy.x), y(xy.y), z(z), w(w) {}
+		Vector(Vector<T, 3>& xyz, T w) : x(xyz.x), y(xyz.y), z(xyz.z), w(w) {}
+		T& operator[](unsigned int component) { return data[component]; };
 	};
-
-	// Common Vector typedefs
-	typedef Vector<float, 2> Vec2;
-	typedef Vector<float, 3> Vec3;
-	typedef Vector<float, 4> Vec4;
 
 	// Templated by type and size Matrix
 	template<typename T, int rows, int cols>
@@ -87,6 +90,10 @@ namespace ZMath
 	{
 		T data[rows][cols];
 	};
+
+	// Common Matrix typedefs
+	typedef Matrix<float, 3, 3> Mat3x3;
+	typedef Matrix<float, 4, 4> Mat4x4;
 
 	// Partial Matrix specializations with .m00 .m01 .row1 .row2 etc.. access
 	template<typename T>
@@ -101,10 +108,15 @@ namespace ZMath
 				  m10, m11, m12,
 				  m20, m21, m22;
 			};
-			struct { Vec3 row1, row2, row3; };
-			struct { Vec3 rows[3]; };
+			struct { Vector<T, 3> row0, row1, row2; };
+			struct { Vector<T, 3> rows[3]; };
 		};
-		Vec3 operator[](unsigned int n) { return rows[n]; }
+		Matrix() 
+		: m00(0), m01(0), m02(0), m10(0), m11(0), m12(0), m20(0), m21(0), m22(0) {}
+		Matrix(T M00, T M01, T M02, T M10, T M11, T M12, T M20, T M21, T M22)
+		: m00(M00), m01(M01), m02(M02), m10(M10), m11(M11), m12(M12), m20(M20), m21(M21), m22(M22) {}
+		//Matrix(Vector<T, 3>& Row0, Vector<T, 3>& Row1, Vector<T, 3>& Row2) : row0(Row0), row1(Row1), row2(Row2) {}
+		Vector<T, 3>& operator[](unsigned int n) { return rows[n]; }
 	};
 
 	template<typename T>
@@ -120,15 +132,19 @@ namespace ZMath
 				  m20, m21, m22, m23,
 				  m30, m31, m32, m33;
 			};
-			struct { Vec4 row1, row2, row3, row4; };
-			struct { Vec4 rows[4]; };
+			struct { Vector<T, 4> row0, row1, row2, row3; };
+			struct { Vector<T, 4> rows[4]; };
 		};
-		Vec4 operator[](unsigned int n) { return rows[n]; }
+		Matrix()
+		: m00(0), m01(0), m02(0), m03(0), m10(0), m11(0), m12(0), m13(0),
+		  m20(0), m21(0), m22(0), m23(0), m30(0), m31(0), m32(0), m33(0) {}
+		Matrix(T M00, T M01, T M02, T M03, T M10, T M11, T M12, T M13,
+			   T M20, T M21, T M22, T M23, T M30, T M31, T M32, T M33)
+		: m00(M00), m01(M01), m02(M02), m03(M03), m10(M10), m11(M11), m12(M12), m13(M13),
+		  m20(M20), m21(M21), m22(M22), m23(M23), m30(M30), m31(M31), m32(M32), m33(M33) {}
+		//Matrix(Vector<T, 4>& Row0, Vector<T, 4>& Row1, Vector<T, 4>& Row2, Vector<T, 4>& Row3) : row0(Row0), row1(Row1), row2(Row2), row3(Row3) {}
+		Vector<T, 4>& operator[](unsigned int n) { return rows[n]; }
 	};
-
-	// Common Matrix typedefs
-	typedef Matrix<float, 3, 3> Mat3x3;
-	typedef Matrix<float, 4, 4> Mat4x4;
 	
 }//end ZMath
 
--- source/PreliminaryTest/main.cpp
+++ source/PreliminaryTest/main.cpp
@@ -3,20 +3,26 @@ using namespace ZMath;
 
 int main()
 {
-	
-	Vec2 v2(1.0f, 2.0f);
-	Vec2 v3(5.0f, 3.0f);
-	Vec2 v4(26.0f);
-	//Vec3 v3();
-	//Vec4 v4();
-	//Mat3x3 m33;
-	//Mat4x4 m44;
+	Vec3 a;
+	a.x = 100.0f;
+	a.y = 200.0f;
+	a.z = 300.0f;
+	a.r = 400.0f;
+	a.g = 500.0f;
+	a.b = 600.0f;
+	a[0] = 1.0f;
+	a[1] = 0.0f;
+	a[2] = 1.0f;
 
-	v2.x = 5.0f;
-	//v3.x = 5.0f;
-	//v4.x = 5.0f;
-	//m33.m22 = 1.0f;
-	//m44.m22 = 1.0f;
+	Vec3 b(0.0f, 0.0f, 1.0f);
+	float f = b[0];
+
+	Vec3 leet(1337.1337f);
+	f = leet[0];
+
+	Vec2 g(1.0f);
+
+	Vec3 fromTwo(g, 3.0f);
 
 	return 0;
 }
